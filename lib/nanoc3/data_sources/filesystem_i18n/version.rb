3d19307457a52f5b213d9dff84ea08779d6bc37e
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2011-05-23 18:31:03 +0200

    update gemspec & Gemfile

--- lib/nanoc3/data_sources/filesystem_i18n/version.rb
+++ lib/nanoc3/data_sources/filesystem_i18n/version.rb
@@ -1,11 +1,10 @@
 # encoding: utf-8
-
 require 'nanoc3'
 
 module Nanoc3
   module DataSources
     class FilesystemI18n < Nanoc3::DataSource
-      Version = '0.1.0'
+      VERSION = '0.1.0'
     end
   end
 end
